1a379d06e275fd25642fa1ebb07657ed154e6ed5
Denis Vasilev <vassdeniss@gmail.com>
2023-12-07 20:13:31 +0200

    Hid plaer, added play button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,12 +2,22 @@
 
 public partial class MainPage : ContentPage
 {
+    private const int UPDATE_INTERVAL_MILLISECONDS = 100;
+
+    private bool _isPlaying = false;
+    private TimeSpan _totalDuration = TimeSpan.Zero;
+    private TimeSpan _currentPosition = TimeSpan.Zero;
+
     public MainPage()
     {
         this.InitializeComponent();
+        this.mediaElement.WidthRequest = DeviceDisplay
+            .Current
+            .MainDisplayInfo
+            .Width;
     }
 
-    private async void UploadButton_OnClicked(object sender, EventArgs e)
+    private async void UploadButton_OnClicked(object? sender, EventArgs e)
     {
         try
         {
@@ -24,6 +34,8 @@ public partial class MainPage : ContentPage
             }
 
             this.mediaElement.Source = result.FullPath;
+            this._totalDuration = this.mediaElement.Duration;
+            this.mediaElement.Play();
         }
         catch (Exception ex)
         {
@@ -31,7 +43,13 @@ public partial class MainPage : ContentPage
         }
     }
 
-    private void MainPage_OnUnloaded(object sender, EventArgs e)
+    private void PlayButton_OnClicked(object? sender, EventArgs e)
+    {
+        this.mediaElement.Play();
+        this._isPlaying = true;
+    }
+
+    private void MainPage_OnUnloaded(object? sender, EventArgs e)
     {
         this.mediaElement.Handler?.DisconnectHandler();
     }

ef92d7297e45fc24266a9061b7500f2e57156410
Denis Vasilev <vassdeniss@gmail.com>
2023-12-07 23:12:07 +0200

    Replaced slider for time stamp

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,11 +2,12 @@
 
 public partial class MainPage : ContentPage
 {
-    private const int UPDATE_INTERVAL_MILLISECONDS = 100;
+    private const int UPDATE_INTERVAL_MILLISECONDS = 1000;
 
-    private bool _isPlaying = false;
+    private bool _isPlaying;
     private TimeSpan _totalDuration = TimeSpan.Zero;
     private TimeSpan _currentPosition = TimeSpan.Zero;
+    private IDispatcherTimer? _timer;
 
     public MainPage()
     {
@@ -31,7 +32,11 @@ public partial class MainPage : ContentPage
 
             this.playButton.IsEnabled = true;
             this.mediaElement.Source = result.FullPath;
-            this._totalDuration = this.mediaElement.Duration;
+            this.mediaElement.MediaOpened += MediaElementOnMediaOpened;
+            this.mediaElement.Play();
+            this._isPlaying = true;
+
+            this._timer = this.SetUpAudioTimer();
         }
         catch (Exception ex)
         {
@@ -39,18 +44,65 @@ public partial class MainPage : ContentPage
         }
     }
 
+    private void MediaElementOnMediaOpened(object? sender, EventArgs e)
+    {
+        IDispatcherTimer? durationTimer = Application.Current?.Dispatcher.CreateTimer();
+        if (durationTimer is null)
+        {
+            return;
+        }
+
+        durationTimer.Interval = TimeSpan.FromSeconds(1);
+        durationTimer.Tick += (_, _) =>
+        {
+            if (this.mediaElement.Duration == TimeSpan.Zero)
+            {
+                return;
+            }
+
+            this._totalDuration = this.mediaElement.Duration;
+            durationTimer.Stop();
+        };
+    }
+
     private void PlayButton_OnClicked(object? sender, EventArgs e)
     {
         if (this._isPlaying)
         {
+            this._timer!.Stop();
             this.mediaElement.Pause();
             this._isPlaying = false;
         }
         else
         {
+            this._timer!.Start();
             this.mediaElement.Play();
             this._isPlaying = true;
         }
+    }
+
+    private IDispatcherTimer? SetUpAudioTimer()
+    {
+        IDispatcherTimer? timer = Application.Current?.Dispatcher.CreateTimer();
+        if (timer is null)
+        {
+            return null;
+        }
+
+        timer.Interval = TimeSpan.FromMilliseconds(UPDATE_INTERVAL_MILLISECONDS);
+
+        timer.Tick += (_, _) =>
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                this._currentPosition = this.mediaElement.Position;
+                this.progressLabel.Text = $"{this._currentPosition:mm\\:ss} / {this._totalDuration:mm\\:ss}";
+            });
+        };
+
+        timer.Start();
+
+        return timer;
     }
 
     private void MainPage_OnUnloaded(object? sender, EventArgs e)

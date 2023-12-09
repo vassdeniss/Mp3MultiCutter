de0b205f1551dc033496c39c534b71453c1feabd
Denis Vasilev <vassdeniss@gmail.com>
2023-12-09 23:55:54 +0200

    Added visualization

---
+++ TimeSpanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Mp3MultiCutter;
+
+public class TimeSpanConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is TimeSpan timeSpan)
+        {
+            return $"{timeSpan.Hours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}";
+        }
+
+        return string.Empty;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
